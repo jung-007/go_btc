abb4a474cb9f6ad8ae13c9741edbc9926e458d72
sunjun_zh <sunjun_zh@163.com>
2022-08-29 20:08:28 +0800

    persistence

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go_btc
 
 go 1.16
+
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
+)
--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"time"
-)
-
 func main() {
 	bc := NewBlockchain()
-	time.Sleep(time.Second * 3)
-	bc.AddBlock("Send 1 BTC to Jung")
-	time.Sleep(time.Second * 3)
-	bc.AddBlock("Send 2 BTC to Jung")
+	defer bc.db.Close()
 
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev.Hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Printf("Timestamp: %d\n", block.Timestamp)
-
-		pow := NewProofOfWork(block)
-		fmt.Printf("POW： %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
